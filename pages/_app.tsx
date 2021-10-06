a90be3d9a1ccd3b212e1f4f59e37e4558f799e7c
kevlee1 <lee.kev@husky.neu.edu>
2021-10-06 03:23:52 -0400

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -14,7 +14,7 @@ function Robes({ Component, pageProps }) {
         `}
       </style>
       <Head>
-        <title>mystictoads.market</title>
+        <title>exploringtoadz.market</title>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
           href="https://fonts.googleapis.com/css2?family=Inconsolata:wght@400;600;700&display=swap"
@@ -22,15 +22,15 @@ function Robes({ Component, pageProps }) {
         />
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:creator" content="@kevlee97" />
-        <meta property="og:url" content="https://mystictoads.market" />
-        <meta property="og:title" content="mystictoads.market" />
+        <meta property="og:url" content="https://exploringtoadz.market" />
+        <meta property="og:title" content="exploringtoadz.market" />
         <link rel="icon" type="image/png" href="/favicon.png" />
         <meta
           property="og:description"
-          content="See the floor price of Toadz with Mysterious Hoodies from the CrypToadz project."
+          content="See the floor price of Explorer Toadz from the CrypToadz project."
         />
         <script
-          data-goatcounter="https://mystictoads.goatcounter.com/count"
+          data-goatcounter="https://explorertoadz.goatcounter.com/count"
           async
           src="//gc.zgo.at/count.js"
         ></script>
